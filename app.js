bf93bc79c884c81990ce8b85df91862a32c7d234
victor-code <vdcloayza@gmail.com>
2024-02-08 01:50:38 -0400

    beta3

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ import Store from "./services/Store";
 import API from "./services/API";
 import "./app.css";
 import { loadData } from "./services/Menu";
-import Router from "./services/Router";
+import Router from "./services/Router.js";
 
-// Link my Web Components 
-import {MenuPage} from './components/MenuPage.js';
+// Link my Web Components
+// MenuPage is an example
+import "./components/MenuPage.js";
+
 window.app = {};
 app.store = Store;
 app.router = Router;
--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,6 +1,11 @@
 export class MenuPage extends HTMLElement{
     constructor(){
-        super(); 
+      super();
+      // prettier-ignore
+      this.innerHTML =//html
+          `
+        <h1>aaaaaaaaaa</h1>
+        `;
     }
 }
 customElements.define("menu-page", MenuPage);
